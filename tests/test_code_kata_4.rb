19e775e958f1ebfa02bbf7b53c6eea16da9db6c6
Harsh Nene <harsh@blimp.co.in>
2018-10-19 17:42:25 +0200

    test child value

--- tests/test_code_kata_4.rb
+++ tests/test_code_kata_4.rb
@@ -4,11 +4,23 @@ require 'test_helper'
 class TestNode < Minitest::Test
     def test_root_node_creation
         node = Node.new(
-            val: 'root',
+            val: 'root'
         )
         result = node.val
         assert_equal('root', result)
         assert_nil node.left
         assert_nil node.right
+    end
+
+    def test_child_node_value
+        node = Node.new(
+            val: 'root',
+            left: Node.new( val: 'left' ),
+            right: Node.new( val: 'right' )
+        )
+        result = node.left.val
+        assert_equal('left', result)
+        result = node.right.val
+        assert_equal('right', result)
     end
 end
